cb799b56d2d9f36d061bc2cd7d021c54f18c3ead
Ayush Kumar <ayush.krsh9@gmail.com>
2024-08-21 23:18:17 +0530

    added models and mongo db connection

--- models/app.cd.go
+++ models/app.cd.go
@@ -7,10 +7,10 @@ import (
 
 type App struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`  // MongoDB ObjectID
-	AppName   string             `bson:"first_name" json:"first_name"`       // User's first name
-	Port      string             `bson:"last_name" json:"last_name"`         // User's last name
+	AppName   string             `bson:"app_name" json:"first_name"`       // User's first name
+	Port      string             `bson:"port" json:"last_name"`         // User's last name
 	Email     string             `bson:"email" json:"email"`                 // User's email address
-	GithubId  string             `bson:"password" json:"-"`                  // User's password (hash it before storing)
+	GithubId  string             `bson:"github" json:"-"`                  // User's password (hash it before storing)
 	CreatedAt primitive.DateTime `bson:"created_at,omitempty" json:"created_at,omitempty"` // Timestamp for when the user was created
 	UpdatedAt primitive.DateTime `bson:"updated_at,omitempty" json:"updated_at,omitempty"` // Timestamp for when the user was last updated
 }
